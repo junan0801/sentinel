153da126926b620863c1940ea1014e3394736d65
zhangjunan <zhangjunan@gmail.com>
2022-09-13 12:14:08 +0800

    实现collector接口

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ func main() {
 }
 */
 
-import (
+/*import (
 	"net/http"
 	"time"
 
@@ -86,4 +86,72 @@ func main() {
 
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":2112", nil)
+}*/
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"io/ioutil"
+	"net/http"
+)
+
+type Data2 struct {
+	// json 转struct  必须以大写字母开头,否者会被忽略,数据类型也要设置正确,否者会取不到值
+	IP          string  `json:"ip"`
+	App         string  `json:"app"`
+	SuccessQps  float64 `json:"successQps"`
+	MachineName string  `json:"machineName"`
+}
+
+type fooCollector struct {
+	fooMetric *prometheus.Desc
+	barMetric *prometheus.Desc
+}
+
+func newFoolCollector() *fooCollector {
+	m1 := make(map[string]string)
+	m1["env"] = "prod"
+	v := []string{"ip", "app", "machineName"}
+	return &fooCollector{
+		fooMetric: prometheus.NewDesc("FF_METRICS", "SHOW metrics a for", nil, nil),
+		barMetric: prometheus.NewDesc("sentinel", "successqps of app from sentinel", v, m1),
+	}
+
+}
+func (collect *fooCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- collect.barMetric
+	ch <- collect.fooMetric
+
+}
+func (collect *fooCollector) Collect(ch chan<- prometheus.Metric) {
+	url := "http://g-sentinel-dashboard.tope365.com/custom/metric/get"
+	resp, _ := http.Get(url)
+	// 因为json 数据key 不固定,使用map 获取
+	dataMap := make(map[string][]Data2)
+	//for range time.Tick(1 * time.Second) {
+	body, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	json.Unmarshal([]byte(body), &dataMap)
+	fmt.Printf("获取到的数据类型是%T\t", dataMap)
+	//fmt.Println(dataMap)
+	for _, v := range dataMap {
+		for _, v1 := range v {
+			ip := v1.IP
+			app := v1.App
+			//successqps := strconv.FormatFloat(v1.SuccessQps, 'g', 2, 32)
+			successqps := v1.SuccessQps
+			machinename := v1.MachineName
+			fmt.Println(ip, app, successqps, machinename)
+			ch <- prometheus.MustNewConstMetric(collect.barMetric, prometheus.GaugeValue, successqps, ip, app, machinename)
+		}
+	}
+
+}
+func main() {
+	foo := newFoolCollector()
+	prometheus.MustRegister(foo)
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":18080", nil)
 }
